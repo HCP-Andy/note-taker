df279501d54a7cdf3bd7d053b48d75fe219564c7
Andrew Mcnally <andrew.mcinally@housecallpro.com>
2022-10-17 10:31:06 -0600

    moved files

---
+++ src/components/NoteCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NoteCard() {
+  return (
+    <div class="card">
+      <div class="card-body">This is some text within a card body.</div>
+    </div>
+  );
+}
+
+export default NoteCard;
---
+++ src/views/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div class="container text-center">
+      <div class="row">
+        <div class="col">Column1</div>
+        <div class="col">Column2</div>
+        <div class="col">Column3</div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
